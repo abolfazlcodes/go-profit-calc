f65dbb5da45cdc50d758fa5d5cba1b3a23dd1dc8
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2025-02-16 22:40:01 +0330

    initialize GO PROFIT CALCULATOR APP practice

---
+++ profit_calculator.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// GOALS OF THE TASK:
+// 1) Validate the user input:
+//    ==> Show error message & exit if invalid input is provided
+//    - No negative numbers
+//    - Not 0
+// 2) Store calculated results into file
+
+func main() {
+	fmt.Printf("Welcome")
+}
